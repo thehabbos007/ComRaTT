b505b2a8f7b5c6b0e221c65ce5cdd244a749a9a8
Ahmad Sattar <thehabbos007@gmail.com>
2024-09-24 12:56:09 +0200

    Initial wasmer infra

---
+++ README.md
@@ -0,0 +1,9 @@
+# Dependencies
+If you use nix, `nix develop` or nix-direnv is enough to install all dependencies.
+
+Otherwise
+- Install wasmer CLI (https://wasmer.io/)
+
+# Running
+
+`dune exec -- ComRaTT [args]`
--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,24 @@
-open ComRaTTlib
+(* pen ComRaTTlib *)
 
-let () = print_endline "Hello, World!"
-let _ = Absyn.CstI 42
+let prog = "prog.wat"
+
+let mock_program =
+  {|
+(module
+  (func $init (param $lhs i32) (param $rhs i32) (result i32)
+    local.get $lhs
+    local.get $rhs
+    i32.add
+  )
+  (export "add" (func $add))
+)
+  |}
+;;
+
+let () =
+  let oc = open_out prog in
+  Printf.fprintf oc "%s" mock_program;
+  close_out oc;
+  let args = Sys.argv |> Array.to_list |> String.concat " " in
+  Sys.command (Printf.sprintf "wasmer %s --invoke init %s" prog args) |> ignore
+;;
