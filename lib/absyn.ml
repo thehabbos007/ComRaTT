9176eceb7f56fe6f1e28ac1e6f182c7e21f043d1
kaep <sebastianholmgaard@live.dk>
2024-09-24 11:57:40 +0200

    error handling on variable lookup and more small examples

--- lib/absyn.ml
+++ lib/absyn.ml
@@ -23,7 +23,10 @@ type value =
 let rec interp x env =
   match x with
   | CstI i -> VInt i
-  | Var v -> Environment.find v env
+  | Var v ->
+    (match Environment.find_opt v env with
+     | Some var -> var
+     | None -> failwith "variable not defined in environment")
   | Lam (param, _, body) -> VClosure (param, body, env)
   | Prim (op, e1, e2) ->
     (match interp e1 env, interp e2 env with
@@ -42,12 +45,26 @@ let rec interp x env =
      | _ -> failwith "Cannot apply to non-closure")
 ;;
 
-let lambda = Lam ("x", TInt, Var "x")
+let id = Lam ("x", TInt, Var "x")
 let empty : value Environment.t = Environment.empty
 let env = Environment.add "y" (VInt 125) empty
-let example = App (lambda, CstI 84)
+let example = App (id, CstI 84)
 let evaluated = interp example env
 let add_example = Prim (Add, CstI 42, CstI 42)
 let add_eval = interp add_example Environment.empty
 let sub_example = Prim (Sub, CstI 42, CstI 42)
 let sub_eval = interp sub_example Environment.empty
+let square_fun = Lam ("x", TInt, Prim (Mul, Var "x", Var "x"))
+let five_squared = interp (App (square_fun, CstI 5)) Environment.empty
+
+let binop_with_defined_variable_does_not_fail =
+  interp (Prim (Add, CstI 42, Var "x")) (Environment.add "x" (VInt 42) Environment.empty)
+;;
+
+let binop_with_undefined_variable_fails =
+  interp (Prim (Add, CstI 42, Var "x")) (Environment.add "y" (VInt 42) Environment.empty)
+;;
+
+let binop_with_lambda_fails =
+  interp (Prim (Add, CstI 42, Lam ("x", TInt, CstI 2))) Environment.empty
+;;
