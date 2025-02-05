63fffde90f861ed62766c60b60e407461dcedcfb
Ahmad Sattar <thehabbos007@gmail.com>
2025-02-05 10:39:35 +0100

    Add errors to parser

--- lib/ast_of_text.ml
+++ lib/ast_of_text.ml
@@ -1,50 +1,55 @@
-open Lexing
+module P = Parser
+module I = Parser.MenhirInterpreter
+module E = MenhirLib.ErrorReports
+module L = MenhirLib.LexerUtil
 
-let pos_string pos =
-  let open Lexing in
-  Printf.sprintf "%d:%d" pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1)
+module Message = struct
+  type t = Parsing_error
+
+  let default_severity _ = Asai.Diagnostic.Error
+
+  let short_code : t -> string = function
+    | Parsing_error -> "parsing-error"
+  ;;
+end
+
+module Reporter = Asai.Reporter.Make (Message)
+module Term = Asai.Tty.Make (Message)
+
+let display_diagnostic = Term.display
+let succeed x = Ok x
+
+let env checkpoint =
+  match checkpoint with
+  | I.HandlingError env -> env
+  | _ -> failwith "Can only get env from HandlingError checkpoint"
 ;;
 
-let parse_and_print lexbuf =
-  try
-    let ast = Parser.prog Lexer.token lexbuf in
-    Result.ok ast
-  with
-  | Lexer.LexError msg ->
-    (let pos = lexbuf.lex_curr_p in
-     Printf.sprintf "Lexing error at %s: %s\n" (pos_string pos) msg)
-    |> Result.error
-  | Parser.Error ->
-    let token = Lexing.lexeme lexbuf in
-    let position = lexbuf.lex_curr_p in
-    let line = position.pos_lnum in
-    (let column = position.pos_cnum - position.pos_bol + 1 in
-     Printf.sprintf
-       "Syntax error at line %d, column %d: Unexpected token '%s'\n"
-       line
-       column
-       token)
-    |> Result.error
+let state checkpoint : int =
+  match I.top (env checkpoint) with
+  | Some (I.Element (s, _, _, _)) -> I.number s
+  | None -> 0
 ;;
 
-let process_stdin () =
-  let lexbuf = Lexing.from_channel stdin in
-  lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = "stdin" };
-  parse_and_print lexbuf
+let fail lexbuf _buffer (checkpoint : _ I.checkpoint) =
+  let message = Parser_messages.message (state checkpoint) in
+  Reporter.fatalf
+    ~loc:(Asai.Range.of_lexbuf lexbuf)
+    Parsing_error
+    "%S"
+    message
+    (Printf.sprintf "%s%s%s%!" message)
 ;;
 
-let ( let* ) = Result.bind
-
-let print_global ({ name; fundef; _ } : Preprocess.global_def) =
-  Printf.fprintf stdout "%s = %s\n" name (Infer.show_typed_expr fundef) |> ignore
+let try_parse filepath =
+  Reporter.run ~emit:display_diagnostic ~fatal:(fun err -> Error err)
+  @@ fun () ->
+  let lexbuf = Lexing.from_channel (open_in filepath) in
+  lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = filepath };
+  let supplier = I.lexer_lexbuf_to_supplier Lexer.token lexbuf in
+  let buffer, supplier = E.wrap_supplier supplier in
+  let checkpoint = Parser.Incremental.prog lexbuf.lex_curr_p in
+  I.loop_handle succeed (fail lexbuf buffer) supplier checkpoint
 ;;
 
-let ast_of_text text =
-  (let lexbuf = Lexing.from_string text in
-   let* processed = parse_and_print lexbuf in
-   let annotated = Infer.infer_all processed in
-   Result.ok annotated)
-  |> Result.map_error (fun x ->
-    print_endline x;
-    x)
-;;
+let parse_from_file = try_parse
