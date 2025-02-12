9430bc90e788fdacafb821a6a05f0a5e7da484eb
Ahmad Sattar <thehabbos007@gmail.com>
2025-02-12 10:47:17 +0100

    Rust flake stuff

--- flake.nix
+++ flake.nix
@@ -3,59 +3,51 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      systems = [ "x86_64-darwin" "aarch64-darwin" "x86_64-linux" ];
-      createDevShell = system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-          inferno-version = "20220603";
-          inferno = pkgs.ocamlPackages.buildDunePackage {
-            pname = "inferno";
-            version = "v${inferno-version}";
-            src = pkgs.fetchFromGitLab {
-              domain = "gitlab.inria.fr";
-              owner = "fpottier";
-              repo = "inferno";
-              rev = "${inferno-version}";
-              sha256 = "sha256-Cq08GbKnyK0CIbj5NQ6RpU3pzrycWbfEj//lde0kYNY=";
-            };
-
-            propagatedBuildInputs = [ pkgs.ocamlPackages.pprint pkgs.ocamlPackages.unionFind ];
-          };
-        in
-        pkgs.mkShell {
-          buildInputs = [
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [(import rust-overlay)];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
+      toolchain = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml).override {
+        extensions = [
+          "rust-src"
+        ];
+      };
+      # nixpkgs.legacyPackages.${system};
+    in
+      with pkgs; {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            # Rust stuff
+            rust-analyzer-unwrapped
+            cargo-flamegraph
             # Nix stuff
             pkgs.nixd
             pkgs.nil
             # Wasm
-	          pkgs.wasmtime
-						# Ocaml with build system
-            pkgs.ocaml
-            pkgs.ocamlPackages.findlib
-            pkgs.dune_3
-            pkgs.ocamlPackages.ocaml-lsp
-            pkgs.ocamlformat
-            pkgs.ocamlPackages.ocamlformat-rpc-lib
-            pkgs.ocamlPackages.utop
-
-            # Our actual pacakge dependencies
-            pkgs.ocamlPackages.ppx_deriving
-            pkgs.ocamlPackages.ppx_expect
-            pkgs.ocamlPackages.ppx_inline_test
-            pkgs.ocamlPackages.menhir
-            pkgs.ocamlPackages.menhirLib
-            pkgs.ocamlPackages.base
-            pkgs.ocamlPackages.asai
-            pkgs.ocamlPackages.ounit2
-            inferno
+            pkgs.wasmtime
           ];
+          buildInputs = [
+            toolchain
+          ];
+          RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
         };
-    in
-    {
-      devShell = nixpkgs.lib.genAttrs systems createDevShell;
-    };
+      });
 }
