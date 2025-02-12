3d19faa2f3356c245c3e1731e89d1c302b076987
Ahmad Sattar <thehabbos007@gmail.com>
2025-02-12 10:35:21 +0100

    Add inferno

--- flake.nix
+++ flake.nix
@@ -11,10 +11,29 @@
       createDevShell = system:
         let
           pkgs = import nixpkgs { inherit system; };
+          inferno-version = "20220603";
+          inferno = pkgs.ocamlPackages.buildDunePackage {
+            pname = "inferno";
+            version = "v${inferno-version}";
+            src = pkgs.fetchFromGitLab {
+              domain = "gitlab.inria.fr";
+              owner = "fpottier";
+              repo = "inferno";
+              rev = "${inferno-version}";
+              sha256 = "sha256-Cq08GbKnyK0CIbj5NQ6RpU3pzrycWbfEj//lde0kYNY=";
+            };
+
+            propagatedBuildInputs = [ pkgs.ocamlPackages.pprint pkgs.ocamlPackages.unionFind ];
+          };
         in
         pkgs.mkShell {
           buildInputs = [
+            # Nix stuff
+            pkgs.nixd
+            pkgs.nil
+            # Wasm
 	          pkgs.wasmtime
+						# Ocaml with build system
             pkgs.ocaml
             pkgs.ocamlPackages.findlib
             pkgs.dune_3
@@ -23,6 +42,7 @@
             pkgs.ocamlPackages.ocamlformat-rpc-lib
             pkgs.ocamlPackages.utop
 
+            # Our actual pacakge dependencies
             pkgs.ocamlPackages.ppx_deriving
             pkgs.ocamlPackages.ppx_expect
             pkgs.ocamlPackages.ppx_inline_test
@@ -31,6 +51,7 @@
             pkgs.ocamlPackages.base
             pkgs.ocamlPackages.asai
             pkgs.ocamlPackages.ounit2
+            inferno
           ];
         };
     in
